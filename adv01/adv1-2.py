6fe53a2c235e89e2ddb8707f8b8fc918a7f369f2
mihirbpi <mihir.borkar@gmail.com>
2023-12-01 16:37:56 -0800

    formatting

--- adv01/adv1-2.py
+++ adv01/adv1-2.py
@@ -2,7 +2,12 @@ from aocd import get_data
 data = get_data(year=2023, day=1)
 inputs = data.split("\n")
 
-nums_dict = {"one" : "1", "two": "2", "three": "3", "four" : "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9", "1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9"} 
+nums_dict = {"one" : "1", "two": "2", "three": "3", 
+             "four" : "4", "five": "5", "six": "6", 
+             "seven": "7", "eight": "8", "nine": "9", 
+             "1": "1", "2": "2", "3": "3", 
+             "4": "4", "5": "5", "6": "6", 
+             "7": "7", "8": "8", "9": "9"} 
 
 answer = 0
 
